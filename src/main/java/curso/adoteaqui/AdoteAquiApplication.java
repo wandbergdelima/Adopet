4ba82ff0ef288e27ff49a0b4bc5df440b1e743ea
Wandberg de Lima <wandberg@gmail.com>
2023-09-28 19:47:23 -0300

    - inicio do curso.

---
+++ src/main/java/curso/adoteaqui/AdoteAquiApplication.java
@@ -0,0 +1,13 @@
+package curso.adoteaqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoteAquiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdoteAquiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/curso/adoteaqui/AdoteAquiApplicationTests.java
@@ -0,0 +1,13 @@
+package curso.adoteaqui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoteAquiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
